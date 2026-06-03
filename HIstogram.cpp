9e9f16248ba3f2eb11e786b3dc78dcabc47d547a
manthangondane19 <manthangondane19@gmail.com>
2026-06-03 23:03:44 +0530

    Added Histogram And TrappingRainWater Implementations

--- HIstogram.cpp
+++ HIstogram.cpp
@@ -1,30 +1,80 @@
 #include<bits/stdc++.h>
 #include<stack>
 #include<vector>
-void histogram( vector<int> s){
+using namespace std;
+int histogram(vector<int> s)
+{
     stack<int> st;
-    int n=s.size();
-    vector<int>left(n);
-    vector<int>right(n);
 
-    for(int i=0;i<n;i++){
-      while(!st.empty()&&s[st.top()]>=s[i]){
+    int n = s.size();
+
+    vector<int> left(n);
+    vector<int> right(n);
+    vector<int>ans(n);
+
+    // Previous Smaller Element
+    for(int i = 0; i < n; i++){
+    
+        while(!st.empty() && s[st.top()] >= s[i]){
+            st.pop();
+        }
+
+        if(st.empty()){
+
+            left[i] = -1;
+        }
+        else
+        {
+            left[i] = st.top();
+        }
+
+        st.push(i);
+    }
+
+    // Clear stack
+    while(!st.empty()){
+    
         st.pop();
-      }
-      if(st.empty()){
-        left[i]=-1;
-      }else{
-        left[i]=st.top();
-      }
-      st.push(i);
     }
 
-    for(int i=0;i<n;i++){
-        cout<<left[i]<<" ";
+    // Next Smaller Element
+    for(int i = n - 1; i >= 0; i--){
+    
+        while(!st.empty() && s[st.top()] >= s[i]){
+        
+            st.pop();
+        }
+
+        if(st.empty()){
+        
+            right[i] = n;
+        }
+        else
+        {
+            right[i] = st.top();
+        }
+
+        st.push(i);
     }
+
+    int maxArea = 0;
+
+    // Calculate maximum area
+    for(int i = 0; i < n; i++){
+    
+        int width=0;
+        int area=0;
+
+         width = right[i] - left[i] - 1;
+
+         area = width * s[i];
+
+       ans[i]=area; 
+    }
+
+    int endans=0;
+    endans=*max_element(ans.begin(),ans.end());
+    return endans;
 }
 
-int main(){
-    vector<int> s={2,1,5,6,2,3};
-    histogram(s);
-}
+
---
+++ Trappingrainwater.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#include<stack>
+#include<vector>
+using namespace std;
+
+void trapping(vector<int>s){
+    int n=s.size();
+
+    //left max
+    vector<int>lefmaxt(n);
+    leftmax[0]=s[0];
+    
+    for(int i=1;i<n;i++){
+        leftmax[i]=max(leftmax[i-1],s[i]);
+    }
+    
+    //rightmax
+    vector<int>rightmax(n);
+    
+    rightmax[n-1]=s[n-1];
+    
+    for(int i=n-2;i>=0;i--){
+        rightmax[i]=max(rightmax[i+1],s[i]);
+    }
+
+    //accumalation of ans
+    vector<int>ans(n);
+
+    for(int i=0;i<n;i++){
+        ans[i]=min(leftmax[i],rightmax[i])-s[i];
+    }
+
+    int endans= accumulate(ans.begin(),ans.end(),0);
+   
+    return endans;
+}
