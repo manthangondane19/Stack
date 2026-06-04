09084f7acc22b15938b02365b55b318dea3b8fb6
Manthan Nitin Gondane <manthangondane19@gmail.com>
2026-06-04 14:07:38 +0530

    Update PGE.cpp

--- PGE.cpp
+++ PGE.cpp
@@ -1,13 +1,13 @@
 #include<bits/stdc++.h>
 #include<stack>
 #include<vector>
-
+using namespace std;
 vector<int> pge(vector<int>s){ //Previous Greater Element
      stack<int> st;
      int n=s.size();
     vector<int> ans(n);
       for(int i=0;i<=n-1;i++){
-    while(!st.empty()&&s[st.top()]<=s[i]){ //just reverse <= i.e >= for previous smallest element
+    while(!st.empty()&&s[st.top()]<=s[i]){ //just reverse <= i.e use >= for previous smallest element
      st.pop();
   }
   if(st.empty()){
